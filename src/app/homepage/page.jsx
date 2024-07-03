321e6276b171e51585c71d135f49f3020c188723
AvnipVerma <vermaaavnip@gmail.com>
2024-07-03 16:50:03 +0530

    card updated

--- src/app/homepage/page.jsx
+++ src/app/homepage/page.jsx
@@ -4,18 +4,22 @@ import React, { useEffect } from 'react';
 import Image from 'next/image';
 import icon from '@/app/assets/image.png';
 import styles from "./card.module.css";
+// import { IonIcon } from 'react-ionicons';
 import Swiper from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
+
+
+
 function Page() {
 
-  
-      
+
+
 
   return (
-    <div className={styles.containerbig}>
+    <div>
       <section id="trending">
         <div className={styles.container}>
           <h3 className={`${styles.textCenter} ${styles.sectionSubheading}`}>--Best Shoes--</h3>
@@ -23,200 +27,239 @@ function Page() {
           <h1 className={`${styles.textCenter} ${styles.sectionHeading}`}>Trending Shoes</h1>
         </div>
         <div className={styles.container}>
-          <div className={`${styles.swiper} trending-slider`}>
-            <div className={styles.swiperWrapper}>
-              
-              <div className={`${styles.swiperSlide} ${styles.trendingSlide}`}>
-                <div className={styles.trendingSlideImg}>
-                  <div className={styles.imageOverlay}></div>
-                  <Image src={icon} alt=""/>
-                </div>
-                <div className={styles.trendingSlideContent}>
-                  <h1 className={styles.shoePrice}>$125</h1>
-                  <div className={styles.trendingSlideContentBottom}>
-                    <h2 className={styles.shoeName}>
+        <div className={`${styles.swiper}  ${styles.trendingslider}`}>
+
+            <div className={styles.swiperwrapper}>
+
+            <div className={`${styles.swiperSlide} ${styles.trendingslide}`}>
+
+                <div className={styles.trendingslideimg}>
+                  <div className={styles.imageoverlay}></div>
+                  <Image src={icon} alt="" 
+                  className={styles.imagestyle}/>
+                 </div>
+                <div className={styles.trendingslidecontent}>
+                  <div className={styles.shoeprice}>$125</div>
+                  <div className={styles.trendingslidecontentbottom}>
+                    <h2 className={styles.shoename}>
                       Nike Jordans
                     </h2>
-                    <h3 className={styles.shoeRating}>
+                    <h3 className={styles.shoerating}>
                       <span>4.5</span>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star-half"></ion-icon>
+                      {/* <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star-half"></IonIcon> */}
                     </h3>
                   </div>
                 </div>
+               
               </div>
-              
-              <div className={`${styles.swiperSlide} ${styles.trendingSlide}`}>
-                <div className={styles.trendingSlideImg}>
-                  <div className={styles.imageOverlay}></div>
-                  <Image src={icon} alt=""/>
-                </div>
-                <div className={styles.trendingSlideContent}>
-                  <h1 className={styles.shoePrice}>$125</h1>
-                  <div className={styles.trendingSlideContentBottom}>
-                    <h2 className={styles.shoeName}>
-                      Nike Jordans 2
+
+            <div className={`${styles.swiperSlide} ${styles.trendingslide}`}>
+
+                <div className={styles.trendingslideimg}>
+                  <div className={styles.imageoverlay}></div>
+                  <Image src={icon} alt="" 
+                  className={styles.imagestyle}/>
+                 </div>
+                <div className={styles.trendingslidecontent}>
+                  <div className={styles.shoeprice}>$125</div>
+                  <div className={styles.trendingslidecontentbottom}>
+                    <h2 className={styles.shoename}>
+                      Nike Jordans
                     </h2>
-                    <h3 className={styles.shoeRating}>
-                      <span>4</span>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                    </h3>
-                  </div>
-                </div>
-              </div>
-              
-              <div className={`${styles.swiperSlide} ${styles.trendingSlide}`}>
-                <div className={styles.trendingSlideImg}>
-                  <div className={styles.imageOverlay}></div>
-                  <Image src={icon} alt=""/>
-                </div>
-                <div className={styles.trendingSlideContent}>
-                  <h1 className={styles.shoePrice}>$125</h1>
-                  <div className={styles.trendingSlideContentBottom}>
-                    <h2 className={styles.shoeName}>
-                      Nike Air Max
-                    </h2>
-                    <h3 className={styles.shoeRating}>
+                    <h3 className={styles.shoerating}>
                       <span>4.5</span>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star-half"></ion-icon>
+                      {/* <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star-half"></IonIcon> */}
                     </h3>
                   </div>
                 </div>
+               
               </div>
-              
-              <div className={`${styles.swiperSlide} ${styles.trendingSlide}`}>
-                <div className={styles.trendingSlideImg}>
-                  <div className={styles.imageOverlay}></div>
-                  <Image src={icon} alt=""/>
-                </div>
-                <div className={styles.trendingSlideContent}>
-                  <h1 className={styles.shoePrice}>$125</h1>
-                  <div className={styles.trendingSlideContentBottom}>
-                    <h2 className={styles.shoeName}>
-                      Nike Sports
+
+            <div className={`${styles.swiperSlide} ${styles.trendingslide}`}>
+
+                <div className={styles.trendingslideimg}>
+                  <div className={styles.imageoverlay}></div>
+                  <Image src={icon} alt="" 
+                  className={styles.imagestyle}/>
+                 </div>
+                <div className={styles.trendingslidecontent}>
+                  <div className={styles.shoeprice}>$125</div>
+                  <div className={styles.trendingslidecontentbottom}>
+                    <h2 className={styles.shoename}>
+                      Nike Jordans
                     </h2>
-                    <h3 className={styles.shoeRating}>
-                      <span>3.5</span>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star-half"></ion-icon>
-                    </h3>
-                  </div>
-                </div>
-              </div>
-              
-              <div className={`${styles.swiperSlide} ${styles.trendingSlide}`}>
-                <div className={styles.trendingSlideImg}>
-                  <div className={styles.imageOverlay}></div>
-                  <Image src={icon} alt=""/>
-                </div>
-                <div className={styles.trendingSlideContent}>
-                  <h1 className={styles.shoePrice}>$125</h1>
-                  <div className={styles.trendingSlideContentBottom}>
-                    <h2 className={styles.shoeName}>
-                      Adidas 
-                    </h2>
-                    <h3 className={styles.shoeRating}>
+                    <h3 className={styles.shoerating}>
                       <span>4.5</span>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star-half"></ion-icon>
+                      {/* <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star-half"></IonIcon> */}
                     </h3>
                   </div>
                 </div>
+               
               </div>
-              
-              <div className={`${styles.swiperSlide} ${styles.trendingSlide}`}>
-                <div className={styles.trendingSlideImg}>
-                  <div className={styles.imageOverlay}></div>
-                  <Image src={icon} alt=""/>
-                </div>
-                <div className={styles.trendingSlideContent}>
-                  <h1 className={styles.shoePrice}>$125</h1>
-                  <div className={styles.trendingSlideContentBottom}>
-                    <h2 className={styles.shoeName}>
-                      Adidas Runners
+
+            <div className={`${styles.swiperSlide} ${styles.trendingslide}`}>
+
+                <div className={styles.trendingslideimg}>
+                  <div className={styles.imageoverlay}></div>
+                  <Image src={icon} alt="" 
+                  className={styles.imagestyle}/>
+                 </div>
+                <div className={styles.trendingslidecontent}>
+                  <div className={styles.shoeprice}>$125</div>
+                  <div className={styles.trendingslidecontentbottom}>
+                    <h2 className={styles.shoename}>
+                      Nike Jordans
                     </h2>
-                      <h3 className={styles.shoeRating}>
-                      <span>3.5</span>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star-half"></ion-icon>
+                    <h3 className={styles.shoerating}>
+                      <span>4.5</span>
+                      {/* <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star-half"></IonIcon> */}
                     </h3>
                   </div>
                 </div>
+               
               </div>
-              
-              <div className={`${styles.swiperSlide} ${styles.trendingSlide}`}>
-                <div className={styles.trendingSlideImg}>
-                  <div className={styles.imageOverlay}></div>
-                  <Image src={icon} alt=""/>
-                </div>
-                <div className={styles.trendingSlideContent}>
-                  <h1 className={styles.shoePrice}>$125</h1>
-                  <div className={styles.trendingSlideContentBottom}>
-                    <h2 className={styles.shoeName}>
-                      Adidas Sneakers
+
+            <div className={`${styles.swiperSlide} ${styles.trendingslide}`}>
+
+                <div className={styles.trendingslideimg}>
+                  <div className={styles.imageoverlay}></div>
+                  <Image src={icon} alt="" 
+                  className={styles.imagestyle}/>
+                 </div>
+                <div className={styles.trendingslidecontent}>
+                  <div className={styles.shoeprice}>$125</div>
+                  <div className={styles.trendingslidecontentbottom}>
+                    <h2 className={styles.shoename}>
+                      Nike Jordans
                     </h2>
-                    <h3 className={styles.shoeRating}>
-                      <span>3</span>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
+                    <h3 className={styles.shoerating}>
+                      <span>4.5</span>
+                      {/* <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star-half"></IonIcon> */}
                     </h3>
                   </div>
                 </div>
+               
               </div>
-              
-              <div className={`${styles.swiperSlide} ${styles.trendingSlide}`}>
-                <div className={styles.trendingSlideImg}>
-                  <div className={styles.imageOverlay}></div>
-                  <Image src={icon} alt=""/>
-                </div>
-                <div className={styles.trendingSlideContent}>
-                  <h1 className={styles.shoePrice}>$125</h1>
-                  <div className={styles.trendingSlideContentBottom}>
-                    <h2 className={styles.shoeName}>
-                      Adidas 2
+
+            <div className={`${styles.swiperSlide} ${styles.trendingslide}`}>
+
+                <div className={styles.trendingslideimg}>
+                  <div className={styles.imageoverlay}></div>
+                  <Image src={icon} alt="" 
+                  className={styles.imagestyle}/>
+                 </div>
+                <div className={styles.trendingslidecontent}>
+                  <div className={styles.shoeprice}>$125</div>
+                  <div className={styles.trendingslidecontentbottom}>
+                    <h2 className={styles.shoename}>
+                      Nike Jordans
                     </h2>
-                    <h3 className={styles.shoeRating}>
-                      <span>2.5</span>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star"></ion-icon>
-                      <ion-icon name="star-half"></ion-icon>
+                    <h3 className={styles.shoerating}>
+                      <span>4.5</span>
+                      {/* <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star-half"></IonIcon> */}
                     </h3>
                   </div>
                 </div>
+               
               </div>
+
+            <div className={`${styles.swiperSlide} ${styles.trendingslide}`}>
+
+                <div className={styles.trendingslideimg}>
+                  <div className={styles.imageoverlay}></div>
+                  <Image src={icon} alt="" 
+                  className={styles.imagestyle}/>
+                 </div>
+                <div className={styles.trendingslidecontent}>
+                  <div className={styles.shoeprice}>$125</div>
+                  <div className={styles.trendingslidecontentbottom}>
+                    <h2 className={styles.shoename}>
+                      Nike Jordans
+                    </h2>
+                    <h3 className={styles.shoerating}>
+                      <span>4.5</span>
+                      {/* <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star-half"></IonIcon> */}
+                    </h3>
+                  </div>
+                </div>
+               
+              </div>
+
+            <div className={`${styles.swiperSlide} ${styles.trendingslide}`}>
+
+                <div className={styles.trendingslideimg}>
+                  <div className={styles.imageoverlay}></div>
+                  <Image src={icon} alt="" 
+                  className={styles.imagestyle}/>
+                 </div>
+                <div className={styles.trendingslidecontent}>
+                  <div className={styles.shoeprice}>$125</div>
+                  <div className={styles.trendingslidecontentbottom}>
+                    <h2 className={styles.shoename}>
+                      Nike Jordans
+                    </h2>
+                    <h3 className={styles.shoerating}>
+                      <span>4.5</span>
+                      {/* <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star"></IonIcon>
+                      <IonIcon name="star-half"></IonIcon> */}
+                    </h3>
+                  </div>
+                </div>
+               
+              </div>
+
+
+
               
+
             </div>
 
-            <div className={styles.trendingSliderControl}>
-              <div className={`${styles.swiperButtonPrev} ${styles.sliderArrow}`}>
+            <div className={styles.trendingslidercontrol}>
+
+            <div className={`${styles.swiperButtonPrev} ${styles.sliderArrow}`}>
                 <ion-icon name="arrow-back-outline"></ion-icon>
               </div>
               <div className={`${styles.swiperButtonNext} ${styles.sliderArrow}`}>
                 <ion-icon name="arrow-forward-outline"></ion-icon>
               </div>
-              <div className={styles.swiperPagination}></div>
+
+              <div className={styles.swiperpagination}></div>
             </div>
           </div>
         </div>
       </section>
+
     </div>
   );
 }
